7d71714fdb9f4cebe948155ea3b28b04cf5fe3b3
Calum <calum.borders@gmail.com>
2024-10-04 15:45:26 +0100

    Uploading fetching files

--- app/home/page.js
+++ app/home/page.js
@@ -1,3 +1,22 @@
+"use client"
+import { useState } from "react";
+import { fetchEvents } from "../lib/data";
+
 export default function Page() {
-  return <p>Home page</p>;
+  const [eventData, setEventData] = useState([]);
+
+  fetchEvents().then((response) => {
+    setEventData(response);
+  });
+
+  
+  return (
+    <div>
+      <p>Home page</p>
+      {eventData.map((event) => {
+       return <p>{event.id}</p>
+        
+      })}
+    </div>
+  );
 }
--- app/lib/data.js
+++ app/lib/data.js
@@ -1,8 +1,25 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://66d83a8437b1cadd8053e72c.mockapi.io/api/",
+});
+
 export async function fetchEvents() {
     try {
-        
+        const response = await instance.get("events")        
+        return response.data
     } 
-    catch {
-
+    catch (error) {
+        if (error.response){
+            console.error("Server responded with an error", error.response.status, error.response.data)
+        } else if (error.request) {
+            console.error("No response recieved", error.request)
+        } else {
+            console.error("Error setting up request", error.message)
+        }
     }
+
 }
+
+
+
