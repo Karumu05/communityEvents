0de34c34730a2c721d9084d23da345f2e7bfc922
Calum <calum.borders@gmail.com>
2024-10-14 15:08:39 +0100

    Created event cards for front page. correctly formatted data.

--- app/home/page.js
+++ app/home/page.js
@@ -1,22 +1,38 @@
-"use client"
-import { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { fetchEvents } from "../lib/data";
+import EventCard from "../UI/home/eventCard";
 
 export default function Page() {
   const [eventData, setEventData] = useState([]);
+  const currentDate = new Date();
+  const todayDateFormat = currentDate.toLocaleDateString();
 
-  fetchEvents().then((response) => {
-    setEventData(response);
-  });
-
-  
+  useEffect(() => {
+    fetchEvents().then((response) => {
+      setEventData(response);
+    });
+  }, []);
   return (
     <div>
-      <p>Home page</p>
-      {eventData.map((event) => {
-       return <p>{event.id}</p>
-        
-      })}
+      <section className="flex flex-row mx-5 mb-5 ">
+        <h1 className="font-semibold uppercase text-black text-3xl">
+          Upcoming Events
+        </h1>
+      </section>
+
+      <section className="flex flex-row flex-wrap gap-6">
+        {eventData.map((event) => {
+          const formattedDate = event.date.match(/^[^T]*/);
+          return (
+            <EventCard
+              image={event.image}
+              name={event.title}
+              date={formattedDate}
+            />
+          );
+        })}
+      </section>
     </div>
   );
 }
