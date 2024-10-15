ee3591b74ce4383f0433d85bf66dbeb88753ebff
Calum <calum.borders@gmail.com>
2024-10-15 14:05:34 +0100

    installed NextUI, Started testing out components on profile page

--- app/home/profile/page.js
+++ app/home/profile/page.js
@@ -1,5 +1,13 @@
+"use client"
+import { Grid } from "@nextui-org/react"
+
 export default function Page() {
     return (
+        <>
         <p>Profile Page</p>
+        <Grid.Container>
+
+        </Grid.Container>
+        </>
     )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { inter } from "./UI/fonts";
 import "./UI/globals.css";
+import { Providers } from "./providers";
 
 export const metadata = {
   title: "Community Events",
@@ -9,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
