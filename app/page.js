609ff26c4915e0f27b57a75edf605f8b183da946
Calum <calum.borders@gmail.com>
2024-10-31 12:49:11 +0000

    home screen issue fix

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ import { redirect } from "next/navigation";
 export default function Page() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>{error.message}</div>;
 
   if (user) {
     redirect("/home");
